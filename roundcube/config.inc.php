39be47fad20099ba6da6dc9792c36e3217ae0d00
Brewster MacKenzie <brewstermackenzie@protonmail.com>
2025-03-23 10:08:22 +0000

    roundcube cert disable check

---
+++ roundcube/config.inc.php
@@ -0,0 +1,7 @@
+$config['imap_conn_options'] = array(
+        'ssl' => array(
+                'verify_peer' => false,
+//              'verify_depth' => 3,
+//              'cafile'       => '/etc/openssl/certs/ca.crt',
+                ),
+);
