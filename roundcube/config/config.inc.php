2cf045e1819492a9208328942153d5fde74ca071
Brewster MacKenzie <brewstermackenzie@protonmail.com>
2025-03-23 12:17:57 +0000

    removed roundcube

--- roundcube/config/config.inc.php
+++
@@ -1,7 +0,0 @@
-$config['imap_conn_options'] = array(
-        'ssl' => array(
-                'verify_peer' => false,
-//              'verify_depth' => 3,
-//              'cafile'       => '/etc/openssl/certs/ca.crt',
-                ),
-);
